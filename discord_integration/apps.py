ea9d95099ea3db0a4bed5d3c9af612b57005f851
Evan <evanzhang1028@hotmail.com>
2021-05-28 18:12:03 -0400

    Support Django 3

--- discord_integration/apps.py
+++ discord_integration/apps.py
@@ -5,3 +5,4 @@ from django.utils.translation import gettext_lazy as _
 class DiscordIntegrationConfig(AppConfig):
     name = 'discord_integration'
     verbose_name = _('Discord Integration')
+    default_auto_field = 'django.db.models.AutoField'
