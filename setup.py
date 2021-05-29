0f23cfe0ab980347efbdc7213997d8911b301fc4
Evan <evanzhang1028@hotmail.com>
2021-05-28 20:07:53 -0400

    Version 1.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='django-discord-integration',
-    version='1.0.4',
+    version='1.0.5',
     author='Evan Zhang',
     install_requires=['requests', 'django-solo'],
     description='Discord integration for Django, supporting error reporting via webhooks.',
